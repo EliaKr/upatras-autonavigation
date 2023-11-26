5acd4e9092af02a893eb269c9f0dba4eb09515d0
Gck2005 <151639623+Gck2005@users.noreply.github.com>
2023-11-26 17:57:48 +0200

    Create find_path.py

---
+++ Kostakis/find_path.py/find_path.py
@@ -0,0 +1,41 @@
+#find_path
+
+
+matrix = [[1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 1, 1], [1, 0, 0, 0, 1, 0, 0, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 'end', 0, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1]]
+start_x = 2
+start_y = 3
+end_x = 3
+end_y = 5
+dimension_x = 4
+dimension_y = 6
+
+def detect_poss():
+    current_x = start_x
+    current_y = start_y
+    can_do_moves=[]
+    for i in range (-1,2):
+        for p in range(-1,2):
+            if matrix[current_x + i][current_y + p]==0:
+                can_do_moves.append((current_x + i, current_y + p))
+    try:
+        can_do_moves.remove((current_x, current_y))
+    except Exception:
+        None
+
+    
+    
+    return can_do_moves
+print(detect_poss())
+
+def find_path():
+    can_do_moves = detect_poss()
+    for i in can_do_moves:
+        x = (can_do_moves[i][0])
+        y = (can_do_moves[i][1])
+        distance = (((end_x - x)**2 + (end_y - y)**2)**0.5)
+        d = {}
+        d.update({can_do_moves[i]: distance})
+    return d
+
+
+print(find_path())
