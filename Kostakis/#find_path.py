293e6769f41d3163bb5c2cb5807ca195de09d96a
EliaKr <ikrigos@gmail.com>
2023-11-26 18:37:46 +0200

    modified:   Kostakis/#find_path.py

--- Kostakis/#find_path.py
+++ Kostakis/#find_path.py
@@ -29,8 +29,8 @@ print(detect_poss())
 def find_path():
     can_do_moves = detect_poss()
     for i in can_do_moves:
-        x = (can_do_moves[i][0])
-        y = (can_do_moves[i][1])
+        x = (can_do_moves[can_do_moves.index(i)][0])
+        y = (can_do_moves[can_do_moves.index(i)][1])
         distance = (((end_x - x)**2 + (end_y - y)**2)**0.5)
         d = {}
         d.update({can_do_moves[i]: distance})
