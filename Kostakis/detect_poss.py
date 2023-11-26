790c6c31afe649668125f3274fb2cc623b43c783
Gck2005 <151639623+Gck2005@users.noreply.github.com>
2023-11-26 18:31:52 +0200

    Add files via upload

---
+++ Kostakis/detect_poss.py
@@ -0,0 +1,20 @@
+
+#επιστρέφει λίστα της μορφής [(x,y)]
+def detect_poss():
+    current_x = start_x
+    current_y = start_y
+    can_do_moves=[]
+    for i in range (-1,2):
+        for p in range(-1,2):
+            if matrix[current_x + i][current_y + p]==0:
+                can_do_moves.append((current_x + i, current_y + p))
+    try:
+        can_do_moves.remove((current_x, current_y))
+    except Exception:
+        None
+
+    
+    
+    return can_do_moves
+
+
