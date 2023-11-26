0c9d2ac5f8ab5a8ab3e0fc24888ec9055daa4091
Gck2005 <151639623+Gck2005@users.noreply.github.com>
2023-11-26 18:32:16 +0200

    Add files via upload

---
+++ Kostakis/find_path.py
@@ -0,0 +1,18 @@
+
+def find_path():
+    can_do_moves = detect_poss()
+    for i in can_do_moves:
+        x = (can_do_moves[i][0])
+        y = (can_do_moves[i][1])
+        distance = (((end_x - x)**2 + (end_y - y)**2)**0.5)
+        d = {}
+        d.update({can_do_moves[i]: distance})
+    return d
+
+
+
+
+
+
+
+
