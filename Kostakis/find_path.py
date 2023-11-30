40b759bd206ca91dc3775fd6ddac8818fa59970f
Gck2005 <151639623+Gck2005@users.noreply.github.com>
2023-11-30 10:30:07 +0200

    Update find_path.py

--- Kostakis/find_path.py
+++ Kostakis/find_path.py
@@ -1,11 +1,11 @@
 
 def find_path(current_x=start_x,current_y=start_y):
     can_do_moves = detect_poss(current_x,current_y)
+    d = {}
     for i in can_do_moves:
         x = i[0]
         y = i[1]
         distance = (((end_x - x)**2 + (end_y - y)**2)**0.5)
-        d = {}
         d.update({can_do_moves[can_do_moves.index(i)] : distance })
     sorted_d = dict(sorted(d.items(), key=lambda x:x[1]))
 
