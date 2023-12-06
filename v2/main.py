3124d5624bb13239c05a4dbf8b58314a4d796bac
EliaKr <ikrigos@gmail.com>
2023-12-06 10:59:00 +0200

    Added unsolvable message

--- v2/main.py
+++ v2/main.py
@@ -254,8 +254,11 @@ def plot(positions, solvingtime_ms = "Unknown", distance_travelled = "Unknown"):
     for i in positions:
         x.append(i[0])
         y.append(i[1])
+    if len(positions) > 1:
+        plt.xlabel(f"Solving Time: {solvingtime_ms}ms, Distance Travelled: {distance_travelled}")
+    else:
+        plt.xlabel(f"Unsolvable")
     
-    plt.xlabel(f"Solving Time: {solvingtime_ms}ms, Distance Travelled: {distance_travelled}")
     plt.plot(x, y, "b-", linewidth=8.0)
     
 def poslist(path, end):
