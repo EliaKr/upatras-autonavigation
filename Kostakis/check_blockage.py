b02565859d5ee7ef2025d56a866ae83cdd981c7f
Gck2005 <151639623+Gck2005@users.noreply.github.com>
2023-12-06 09:37:05 +0200

    Update check_blockage.py

--- Kostakis/check_blockage.py
+++ Kostakis/check_blockage.py
@@ -1,16 +1,13 @@
-def check_blockage( next_point[0],  next_point[1], current_x, current_y):
+def check_blockage( next_point_x,  next_point_y, current_x, current_y):
     available_points = []
-    z = detect_poss(next_point[0], next_point[1]) 
+    z = detect_poss(next_point_x, next_point_y) 
     z.remove((current_x, current_y))
-    if not (next_point[0], next_point[1] + 1) and (next_point[0], next_point[1] - 1) and (next_point[0] + 1, next_point[1]) and (next_point[0] - 1, next_point[1]) in z:
-        return (next_point[0], next_point[1])
+    if not (next_point_x, next_point_y + 1) and (next_point_x, next_point_y - 1) and (next_point_x + 1, next_point_y) and (next_point_x - 1, next_point_y) in z:
+        return (next_point_x, next_point_y)
     else:
         return None
 
 
-
-    
-        
 
 
 
