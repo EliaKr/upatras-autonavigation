16fe9e4467de5ef8ca3798326609697fe6b2b18e
EliaKr <ikrigos@gmail.com>
2023-12-12 16:47:42 +0200

    renamed:    Kostakis/file.py -> Kostakis/savefile.py

--- Kostakis/savefile.py
+++ Kostakis/savefile.py
@@ -1,8 +1,8 @@
 import datetime
-def file(matrix):
+def savefile(matrix):
     current_datetime = datetime.datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S") 
-    f = open(f"matrix_{formatted_datetime}.txt", 'a', encoding = 'utf-8' )
+    f = open(f"matrix_{formatted_datetime}.txt", 'w', encoding = 'utf-8' )
     f.write(str(matrix))
     f.close
     print (f.name)
