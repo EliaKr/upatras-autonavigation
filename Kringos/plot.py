2a2d3261dfd1d0d6120d39460b513a1d81c7ccfb
EliaKr <ikrigos@gmail.com>
2023-12-12 16:37:20 +0200

    added kostakisv1

--- Kringos/plot.py
+++ Kringos/plot.py
@@ -1,14 +1,15 @@
 # Παίρνει είσοδο της μορφής positions = [(2, 2), (2, 3), (2, 4), (3, 4), (4, 4)]
 # χρόνος σε ms
 
-def plot(positions, solvingtime, distance_travelled):
+def plot(positions, solvingtime_ms = "Unknown", distance_travelled = "Unknown"):
     x = []
     y = []
     for i in positions:
         x.append(i[0])
         y.append(i[1])
+    if len(positions) > 1:
+        plt.xlabel(f"Solving Time: {solvingtime_ms}ms, Distance Travelled: {distance_travelled}")
+    else:
+        plt.xlabel(f"Unsolvable")
     
-    plt.xlabel(f"Solving Time: {solvingtime}ms, Distance Travelled: {distance_travelled}")
-    plt.plot(x, y, "b--", linewidth=8.0)
-    
-plot(positions, solving_time, dist)
+    plt.plot(x, y, "b-", linewidth=8.0)
